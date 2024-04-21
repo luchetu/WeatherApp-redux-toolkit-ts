2af1f89953522961dbc458f34ed67b19bb1e8dc3
Luchetu <admin@EBLLICTR7NPWQC.local>
2024-04-21 15:29:31 +0300

    search on button click.Removed search on state change

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ const App: React.FC = () => {
 
   useEffect(() => {
     getCoordinates();
-  }, [getCoordinates]);
+  }, []);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
